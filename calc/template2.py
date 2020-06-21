d761acd8f99601e07f7a2e1a048f047a068f2d0d
heegu123 <kookminkhg@kookmin.ac.kr>
2020-06-22 01:12:03 +0900

    Issue #3 Uploaded Week11 Homework

---
+++ calc/template2.py
@@ -0,0 +1,16 @@
+html = b"""
+<html>
+    <body>
+        <form method="get" action="">
+            a : <input type = "number" name = "a">	
+            b : <input type = "number" name = "b">
+            <input type="submit">
+        </form>
+	<p>
+	%(sum)s</br>
+	%(mul)s</br>
+	<p>
+    </body>
+</html>
+"""
+
