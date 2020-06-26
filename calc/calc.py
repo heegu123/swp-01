cc3258b69b53cdba8ead4790ab435707a3227714
heegu123 <kookminkhg@kookmin.ac.kr>
2020-06-26 19:43:15 +0900

    Issue #8 수정1 : UnboundLocalError해결위해 sum과 mul의 초기값을 각각 a+b, a*b로 설정해두었습니다. 수정2 : calc.py의 line 11 이 예외 상황유형(2)를 만족시키므로 예외상황(2)가 발생하였을때 출력될 입력된 값이 정수가 아님을 알려주는 에러메시지를 추가하였습니다. 수정3 : if 문을 추가하여 예외상황(3)을 만족시키는 경우에 어떤 값이 입력되지 않았는지 알려주고,  잘못입력된 값을 알려주는 에러메시지를 추가하였습니다.

--- calc/calc.py
+++ calc/calc.py
@@ -6,16 +6,33 @@ def application(environ, start_response):
 
     a = d.get('a', [''])[0]
     b = d.get('b', [''])[0]
-    error = ""
+    sum, mul = "a+b", "a*b"
+    error = " "
     if '' not in [a,b]:
-        a, b = int(a), int(b)
-	sum = a+b
-	mul = a*b
+        if a.isdigit() and b.isdigit() :
+            a, b = int(a), int(b)
+            sum = a+b
+            mul = a*b
+        else:
+            sum, mul = '', ''
+            for i in [a, b]:
+                if i.isdigit()==False:
+                    error += "Error : %s is not integer\n" %i
     elif a=='' and b=='':
         sum = ''
         mul = ''
-	error = "Nothing Entered"
-    response_body = html % {'sum':sum, 'mul':mul, 'error':error}
+        error = "Nothing Entered"
+    elif a=='' and b!='':
+        sum, mul = '', ''
+        error += "Error : a is not entered\n"
+        if b.isdigit()==False:
+            error += "Error : %s is not integer" %b
+    elif a!='' and b=='':
+        sum, mul = '', ''
+        error += "Error : b is not entered\n"
+        if a.isdigit()==False:
+            error += "Error : %s is not integer" %b
+    response_body = html % {'sum':sum, 'mul':mul,'error':error}
     start_response('200 OK', [
         ('Content-Type', 'text/html'),
         ('Content-Length', str(len(response_body)))
