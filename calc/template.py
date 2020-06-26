54989e37aef465c63ea1254491c3b0cdd1cbbec7
heegu123 <kookminkhg@kookmin.ac.kr>
2020-06-26 17:25:28 +0900

    Issue #7 a, b값이 입력되지 않았을 경우, sum, mul값을 공백으로 초기화 하고 합, 곱 아래에 따로 입력되지 않았다는 메시지가 출력하도록 변경하였습니다. 출력값과 입력값을 더 보기 편하게 템플릿에서 줄바꿈을 추가하였습니다.

--- calc/template.py
+++ calc/template.py
@@ -2,13 +2,14 @@ html = b"""
 <html>
     <body>
         <form method="get" action="">
-            a = <input type = "number" name = "a">	
-            b = <input type = "number" name = "b">
+            a = <input type = "number" name = "a"><br><br>	
+            b = <input type = "number" name = "b"><br><br>
             <input type="submit">
         </form>
 	<p>
-	Sum = %(sum)d<br><br>
-	Mul = %(mul)d
+	Sum(a+b) = %(sum)s<br><br>
+	Mul(a*b) = %(mul)s<br><br>
+        %(error)s
 	<p>
     </body>
 </html>
